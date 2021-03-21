82723f2c90b6b3bed0a8a8e998d764825c7453cb
AlexandrCh04 <a.chertikhin0072@mail.ru>
2021-03-21 16:27:06 +0500

    lesson 2

---
+++ script.js
@@ -0,0 +1,61 @@
+const goodsList = document.querySelector('.goods-list');
+
+class BinItem {
+	constructor(title, price) {
+		this.title = title;
+		this.price = price;
+	}
+
+	render() {
+		return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price} RUB</p><button>Add</button></div>`
+	}
+}
+
+class Bin {
+	constructor() {
+		this.goods = [];
+	}
+
+	fetchGoods() {
+		this.goods = [{
+				title: 'Shirt',
+				price: 150
+			},
+			{
+				title: 'Socks',
+				price: 50
+			},
+			{
+				title: 'Jacket',
+				price: 350
+			},
+			{
+				title: 'Shoes',
+				price: 250
+			}
+		]
+	}
+
+	render() {
+		let listHtml = ``;
+		this.goods.forEach(good => {
+			const goodItem = new BinItem(good.title, good.price);
+			listHtml += goodItem.render();
+		});
+		goodsList.innerHTML = listHtml;
+	}
+
+	summ() {
+		let summ = 0;
+		this.goods.forEach(good => {
+			summ += good.price;
+		});
+		const total = document.querySelector('.goods-total');
+		total.textContent = `Total: ${summ} RUB`;
+	}
+}
+
+const list = new Bin();
+list.fetchGoods();
+list.render();
+document.querySelector('#summ').addEventListener('click', () => list.summ())
